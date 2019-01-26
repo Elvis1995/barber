f8b81440d82adf963b47fe2ebd6828b1d3ac5b35
Elvis ajdarpasic <ajdarpasic_95@hotmail.com>
2019-01-26 17:24:19 +0100

    Staring on nav

--- barber_examen/src/App.js
+++ barber_examen/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import Header from './components/header/Header'
+import Nav from './components/Nav/Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Header/>
+		  <Nav/>
+		  <h1>hej</h1>
+		  <h1>hej</h1>
+		  <h1>hej</h1>
+		  <h1>hej</h1>
+		  <h1>hej</h1>
+		  <h1>hej</h1>
+		  <h1>hej</h1>
+		  <h1>hej</h1>
+		  <h1>hej</h1>
+		  <h1>hej</h1>
+		  <h1>hej</h1>
       </div>
     );
   }
