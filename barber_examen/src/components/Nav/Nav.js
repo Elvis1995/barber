18f56f0503305af13b22491dd1a57c643c387e11
Elvis ajdarpasic <ajdarpasic_95@hotmail.com>
2019-01-28 20:09:20 +0100

    Nav transition fixed

--- barber_examen/src/components/Nav/Nav.js
+++ barber_examen/src/components/Nav/Nav.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
-import './nav_func';
 import '../../../src/Master.css';
 import '../../components/Nav/Nav.css';
 
 export default class Nav extends Component {
+	constructor() {
+		super();
+		this.state = {
+			scrolled: false,
+		}
+	}
 
+	componentDidMount() {
+		window.addEventListener('scroll', () => {
+			const isTop = window.scrollY < 10;
+			if(isTop !== true) {
+				this.setState({ scrolled: true });
+			} else {
+				this.setState({ scrolled: false });
+			}
+		});
+	}
+
+	componentWillUnmount () {
+		window.removeEventListener('scroll')
+	}
 
   render() {
     return (
 	  <div>
-		<nav>
+		<div className={this.state.scrolled ? 'nav scrolled' : 'nav'}>
 			<div className="wrapper">
 				<a href="{{ url('intro') }}">
 					<img className="logo" src="/img/logo/logo.png" alt="Logo" />
@@ -20,7 +39,7 @@ export default class Nav extends Component {
 					<a href="{{ url('stores') }}" className="button">Kontakt</a>
 				</div>
 			</div>
-		</nav>
+		</div>
       </div>
     )
   }
