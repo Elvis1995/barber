58190e5030305008a9136b8a8ba5bc413d272f96
TokAhmet <ahmet.ordulu@hotmail.se>
2019-01-30 11:15:14 +0100

    linked image to homepage

--- barber_examen/src/components/Nav/Nav.js
+++ barber_examen/src/components/Nav/Nav.js
@@ -32,11 +32,11 @@ export default class Nav extends Component {
 	  <div>
 		<div className={this.state.scrolled ? 'nav scrolled' : 'nav'}>
 			<div className="wrapper">
-				<a href="{{ url('intro') }}">
+				<Link to={"/"}>
 					<img className="logo" src="/img/logo/logo.png" alt="Logo" />
-				</a>
+				</Link>
 					<div className="menu_container">
-						<Link exact to={"/"} className="button">Startsida</Link>
+						<Link to={"/"} className="button">Startsida</Link>
 						<Link to={"/Boka"} className="button">Boka nu</Link>
 						<Link to={"/Kontakt"} className="button">Kontakt</Link>
 					</div>
