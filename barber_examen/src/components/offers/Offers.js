f2f18e8c1b0c1f00ebc4629db4bc7b37040df4a9
Elvis ajdarpasic <ajdarpasic_95@hotmail.com>
2019-01-31 21:33:06 +0100

    Images and text added to offer section

--- barber_examen/src/components/offers/Offers.js
+++ barber_examen/src/components/offers/Offers.js
@@ -13,31 +13,42 @@ export default class Offers extends Component {
 						<div className="skew">
 							<h2>Herr</h2>
 						</div>
+						<div className="price">
+							<h2>199kr</h2>
+						</div>
 						<div className="text">
-						<h1>aa</h1>
-						<h1>aa</h1>
-						<h1>aa</h1>
-						<h1>aa</h1>
-						<h1>aa</h1>
-						<h1>aa</h1>
-						<h1>aa</h1>
-						<h1>aa</h1>
+							<h5>Klippning</h5>
+							<h5>Rakning</h5>
+							<h5>Styling</h5>
+							<a href="{{ }}" className="button rounded green" data-booking-button>Boka nu</a>
 						</div>
 					</div>
 					<div className="col col_b">
 						<div className="skew">
 							<h2>Dam</h2>
 						</div>
+						<div className="price">
+							<h2>299kr</h2>
+						</div>
 						<div className="text">
-						<h2>hehehhe</h2>
+							<h5>Klippning</h5>
+							<h5>Hårfärgning</h5>
+							<h5>Styling</h5>
+							<h5>hårförlängning</h5>
+							<a href="{{ }}" className="button rounded green" data-booking-button>Boka nu</a>
 						</div>
 					</div>
 					<div className="col col_c">
 						<div className="skew">
 							<h2>Barn</h2>
 						</div>
+						<div className="price">
+							<h2>99kr</h2>
+						</div>
 						<div className="text">
-							<h2>hehehhehe</h2>
+							<h5>Klippning</h5>
+							<h5>Styling</h5>
+							<a href="{{ }}" className="button rounded green" data-booking-button>Boka nu</a>
 						</div>
 					</div>
 				</div>
