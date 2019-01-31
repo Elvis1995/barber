3dc47ee96555de6980a1a8d35f714e5cd7a1fc80
Elvis ajdarpasic <ajdarpasic_95@hotmail.com>
2019-01-31 10:29:08 +0100

    Progress on offers setion

--- barber_examen/src/components/offers/Offers.js
+++ barber_examen/src/components/offers/Offers.js
@@ -8,7 +8,39 @@ export default class Offers extends Component {
 	  <div>
 		<section id="offers">
 			<div className="wrapper">
-				
+				<div className="cols cols_three">
+					<div className="col col_a">
+						<div className="skew">
+							<h2>Herr</h2>
+						</div>
+						<div className="text">
+						<h1>aa</h1>
+						<h1>aa</h1>
+						<h1>aa</h1>
+						<h1>aa</h1>
+						<h1>aa</h1>
+						<h1>aa</h1>
+						<h1>aa</h1>
+						<h1>aa</h1>
+						</div>
+					</div>
+					<div className="col col_b">
+						<div className="skew">
+							<h2>Dam</h2>
+						</div>
+						<div className="text">
+						<h2>hehehhe</h2>
+						</div>
+					</div>
+					<div className="col col_c">
+						<div className="skew">
+							<h2>Barn</h2>
+						</div>
+						<div className="text">
+							<h2>hehehhehe</h2>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
       </div>
