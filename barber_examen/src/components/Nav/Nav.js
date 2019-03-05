e16bd3492095bd0bd3af43bbc85d38e859526329
Elvis ajdarpasic <ajdarpasic_95@hotmail.com>
2019-03-05 15:29:58 +0100

    Nav: Styled responsive

--- barber_examen/src/components/Nav/Nav.js
+++ barber_examen/src/components/Nav/Nav.js
@@ -35,11 +35,23 @@ export default class Nav extends Component {
 				<Link to={"/"}>
 					<img className="logo" src="/img/logo/logo.png" alt="Logo" />
 				</Link>
-					<div className="menu_container">
+				<div className="links">
+					<ul>
 						<Link to={"/"} className="button">Startsida</Link>
 						<Link to={"/Boka"} className="button">Boka nu</Link>
 						<Link to={"/Kontakt"} className="button">Kontakt</Link>
-					</div>
+					</ul>
+				</div>
+				<div className="hamburger">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+				<div className="menu_container">
+					<Link to={"/"} className="button">Startsida</Link>
+					<Link to={"/Boka"} className="button">Boka nu</Link>
+					<Link to={"/Kontakt"} className="button">Kontakt</Link>
+				</div>
 			</div>
 		</div>
       </div>
