00e19bdaa4bc123d4dbc401dc93eca0c2893851e
TokAhmet <ahmet.ordulu@hotmail.se>
2019-03-14 11:35:43 +0100

    some minor fixes and improvments

--- barber_examen/src/App.js
+++ barber_examen/src/App.js
@@ -3,6 +3,7 @@ import Homepage from './components/homepage/Homepage';
 import Contactpage from './components/contactpage/Contactpage';
 import Bookingpage from './components/bookingpage/Bookingpage';
 import {BrowserRouter as Router , Route} from 'react-router-dom';
+import ScrollToTop from './Scrolltotop';
 
 let homePage = () => {
   return(
@@ -27,11 +28,13 @@ class App extends Component {
   render() {
     return (
       <Router>
+      <ScrollToTop>
         <div>      
           <Route exact={true} path="/" component={homePage} />
           <Route path="/Boka" component={BookingPage} />
           <Route path="/Kontakt" component={contact} />
         </div>
+      </ScrollToTop>
       </Router>
     );
   }
